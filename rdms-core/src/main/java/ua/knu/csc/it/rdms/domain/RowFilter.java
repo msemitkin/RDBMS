98319fc7e45c5dcb216838479eecc70f8d449baf
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-10-02 15:29:36 +0300

    reduced duplication

--- rdms-core/src/main/java/ua/knu/csc/it/rdms/domain/RowFilter.java
+++ rdms-core/src/main/java/ua/knu/csc/it/rdms/domain/RowFilter.java
@@ -1,29 +1,18 @@
 package ua.knu.csc.it.rdms.domain;
 
-import ua.knu.csc.it.rdms.domain.column.Column;
-
 import java.util.Map;
 import java.util.function.Predicate;
 
 public record RowFilter(Map<String, Predicate<Object>> nameToFilter) {
 
     public boolean matches(Row row) {
-        Map<Column, Object> rowValues = row.values();
         return nameToFilter.entrySet().stream()
             .allMatch(entry -> {
                 String columnName = entry.getKey();
-                Object columnValue = getValueByColumnName(rowValues, columnName);
+                Object columnValue = row.getValueByColumnName(columnName);
                 Predicate<Object> columnFilter = entry.getValue();
                 return columnFilter.test(columnValue);
             });
-    }
-
-    private Object getValueByColumnName(Map<Column, Object> rowValues, String columnName) {
-        return rowValues.entrySet().stream()
-            .filter(columnToValue -> columnToValue.getKey().name().equals(columnName))
-            .map(Map.Entry::getValue)
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("Invalid column name: %s".formatted(columnName)));
     }
 
 }

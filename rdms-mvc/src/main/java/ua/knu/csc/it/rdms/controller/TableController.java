784a9859e99c4882066fdedcd9c35fdb635314db
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-10-04 23:47:43 +0300

    added table delete button

--- rdms-mvc/src/main/java/ua/knu/csc/it/rdms/controller/TableController.java
+++ rdms-mvc/src/main/java/ua/knu/csc/it/rdms/controller/TableController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import ua.knu.csc.it.rdms.domain.Row;
 import ua.knu.csc.it.rdms.domain.TableSchema;
 import ua.knu.csc.it.rdms.dto.RowDto;
@@ -33,8 +34,16 @@ public class TableController {
         List<RowDto> rowDtos = rows.stream()
             .map(RowMapper::toDto)
             .toList();
+        model.addAttribute("databaseName", database);
+        model.addAttribute("tableName", table);
         model.addAttribute("columns", TableSchemaMapper.toDto(tableSchema).getColumns());
         model.addAttribute("rows", rowDtos);
         return "table";
+    }
+
+    @PostMapping("/databases/{database}/tables/{table}/delete")
+    public String deleteTable(@PathVariable String database, @PathVariable String table) {
+        databaseManager.dropTable(database, table);
+        return "redirect:/databases/%s".formatted(database);
     }
 }

ad13e9f23459464958ae4772dbd5918ef67e79f1
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-10-05 22:59:11 +0300

    added insert button

--- rdms-mvc/src/main/java/ua/knu/csc/it/rdms/controller/TableController.java
+++ rdms-mvc/src/main/java/ua/knu/csc/it/rdms/controller/TableController.java
@@ -3,22 +3,35 @@ package ua.knu.csc.it.rdms.controller;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import ua.knu.csc.it.rdms.domain.Row;
 import ua.knu.csc.it.rdms.domain.SortDirection;
 import ua.knu.csc.it.rdms.domain.Sorting;
 import ua.knu.csc.it.rdms.domain.TableSchema;
 import ua.knu.csc.it.rdms.domain.column.Column;
+import ua.knu.csc.it.rdms.domain.column.columntype.CharColumnType;
+import ua.knu.csc.it.rdms.domain.column.columntype.ColumnType;
+import ua.knu.csc.it.rdms.domain.column.columntype.DoubleColumnType;
+import ua.knu.csc.it.rdms.domain.column.columntype.EmailColumnType;
+import ua.knu.csc.it.rdms.domain.column.columntype.Enumeration;
+import ua.knu.csc.it.rdms.domain.column.columntype.IntegerColumnType;
+import ua.knu.csc.it.rdms.domain.column.columntype.StringColumnType;
 import ua.knu.csc.it.rdms.dto.RowDto;
 import ua.knu.csc.it.rdms.mapper.RowMapper;
 import ua.knu.csc.it.rdms.mapper.TableSchemaMapper;
 import ua.knu.csc.it.rdms.port.input.DatabaseManager;
+import ua.knu.csc.it.rdms.port.input.InsertRowCommand;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toMap;
 
 @Controller
 public class TableController {
@@ -62,6 +75,17 @@ public class TableController {
         return "redirect:/databases/%s".formatted(database);
     }
 
+    @PostMapping("/databases/{database}/tables/{table}/insert")
+    public String insert(
+        @PathVariable String database,
+        @PathVariable String table,
+        @RequestBody MultiValueMap<String, Object> formData
+    ) {
+        TableSchema tableSchema = databaseManager.getTableSchema(database, table);
+        databaseManager.insert(database, table, new InsertRowCommand(castMap(formData, tableSchema)));
+        return "redirect:/databases/%s/tables/%s".formatted(database, table);
+    }
+
     private List<Row> getRows(
         String database,
         String table,
@@ -74,4 +98,69 @@ public class TableController {
         }
         return databaseManager.selectAllRows(database, table);
     }
+
+    public Map<String, Object> castMap(MultiValueMap<String, Object> formData, TableSchema tableSchema) {
+        Map<String, Object> stringObjectMap = formData.toSingleValueMap();
+        return tableSchema.columns().stream()
+            .collect(toMap(
+                Column::name,
+                column -> {
+                    Object value = stringObjectMap.get(column.name());
+                    return castTo(value, column.type());
+                }));
+    }
+
+    private Object castTo(Object object, ColumnType to) {
+        if (to instanceof IntegerColumnType) {
+            return castToInt(object);
+        } else if (to instanceof CharColumnType) {
+            return castToChar(object);
+        } else if (to instanceof DoubleColumnType) {
+            return castToDouble(object);
+        } else if (to instanceof StringColumnType) {
+            return object.toString();
+        } else if (to instanceof EmailColumnType) {
+            return object.toString();
+        } else if (to instanceof Enumeration) {
+            return object.toString();
+        } else {
+            throw new IllegalArgumentException("Not supported type");
+        }
+    }
+
+    private Object castToChar(Object object) {
+        if (object instanceof Character ch) {
+            return ch;
+        } else if (object.toString().length() == 1) {
+            return object.toString().charAt(0);
+        } else {
+            throw new IllegalArgumentException("Not supported type");
+        }
+    }
+
+    private Integer castToInt(Object object) {
+        try {
+            if (object instanceof Integer integer) {
+                return integer;
+            } else {
+                return Integer.parseInt(object.toString());
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Not supported type");
+        }
+    }
+
+    private Double castToDouble(Object object) {
+        try {
+            if (object instanceof Double dbl) {
+                return dbl;
+            } else {
+                return Double.parseDouble(object.toString());
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Not supported type");
+        }
+    }
+
+
 }
